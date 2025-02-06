d54a8bce030c450ee4714a384e04e4093648c8da
Sean <seasushi123@gmail.com>
2025-02-06 12:26:21 -0500

    Create J1.py

---
+++ 2024 Junior Canadian Computing Competition/J1.py
@@ -0,0 +1,10 @@
+#Each red plate of sushi costs $3, each green plate of sushi costs $4, and each blue plate of sushi costs $5
+r = int(input())
+g = int(input())
+b = int(input())
+
+rc = r*3
+gc = g*4
+bc = b*5
+total = rc + gc + bc
+print(total)
