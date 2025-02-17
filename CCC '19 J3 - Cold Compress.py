2f41daca6195c432d23956fa9d027bd66b702118
Sean <seasushi123@gmail.com>
2025-02-17 12:26:15 -0500

    Update CCC '19 J3 - Cold Compress.py

--- CCC '19 J3 - Cold Compress.py
+++ CCC '19 J3 - Cold Compress.py
@@ -1,1 +1,20 @@
+def another(string,char):
+    if len(string) < 1:
+        return 2
+    else:
+        if char == string[0]:
+            return 1, string[1:]
+        else:
+            return 0, string[1:]
+reps = int(input)
+strings = []
 
+for i in range(reps):
+    temp = input("")
+    length = len(temp)
+    counter = -1
+    for x in temp:
+        counter += 1
+        current = x
+        if temp[counter+1] == x:
+            pass
